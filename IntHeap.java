68ccb19203ce368615bba09c2b3ac5575e3fcea6
Jia Xun <cjx3711@yahoo.com>
2014-11-30 15:22:53 +0800

    Added IntHeap
    IntHeap skeleton has been created.
    Functions not implemented

---
+++ IntHeap.java
@@ -0,0 +1,20 @@
+
+/**
+ * Heap class to do heaps!
+ * @author Jia Xun
+ */
+class IntHeap {
+	int [] data; //Stores data
+
+	/**
+	 * Insert
+	 * @param n Number to insert.
+	 */
+	public void insert (int n) {
+
+	}
+	public void remove (int n) {
+
+	}
+	public int [] sort ()
+}
