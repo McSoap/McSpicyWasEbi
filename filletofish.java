1d842cbead699d5efbf6bd40e14f79e0bbc026b3
atomz520 <imsq117@hotmail.com>
2014-11-30 16:48:02 +0800

    fillet

--- filletofish.java
+++ filletofish.java
@@ -1,15 +1,11 @@
 
 public class Hello {
-    public static int findMax ( int px, int py )
-    {
-        return px >= py ? px : py;
-    }
 
 	public static void main(String[] args){
-		int num=0;
-		for(int i=0;i<1000;i++){
-			num++;
-			System.out.println(num);
+		int[] array;
+		array=new int[10];
+		for(int i=0;i<10;i++){
+			System.out.println(array[i]);
 		}
 	}
 }
