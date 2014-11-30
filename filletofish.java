52e13658623393865da006fc57ca175064c038ef
glutSolidSphere <lee.w.zheng@gmail.com>
2014-11-30 15:27:37 +0800

    filletofish.java

--- filletofish.java
+++ filletofish.java
@@ -1,5 +1,10 @@
 
 public class Hello {
+    public static int findMax ( int px, int py )
+    {
+        return px >= py ? px : py;
+    }
+
 	public static void main(String[] args){
 		int num=0;
 		for(int i=0;i<10000000;i++){
